4e41b6db0c35108f0a120ce145b4122bc1d82ab0
saavedrajl <jlds161089@gmail.com>
2016-03-29 09:39:16 -0500

    Seteando una variable de configuracion y agregando registros

--- exceljs-example02.js
+++ exceljs-example02.js
@@ -4,10 +4,21 @@ var Promise = require('bluebird');
 var Excel = require('exceljs');
 var fs = require('fs');
 
-var data = [];
 var conf = {
-	FROM: 'tercero.xlsx',
-	TO: 'resultado.xlsx'
+	PATH: 'resultado.xlsx',
+	COLS: [    
+		{ header: 'Id', key: 'id', width: 10 },
+	    { header: 'Name', key: 'name', width: 35 },
+	    { header: 'Email', key: 'email', width: 20 },
+	    { header: 'Ubigeo', key: 'ubigeo', width: 20 }
+    ],
+	ROWS: [
+	    [1, 'Jose Damian', '<jdamian@tam-c.com>', 'Pucallpa'],
+	    [2, 'Gustabo Artica', '<gartica@tam-c.com>', 'Huancayo'],
+	    [3, 'Diana Hun', '<diana.hun@tam-c.com>', 'Lima'],
+	    [4, 'Juan Azabache', '<jazabache@tam-c.com>', 'Lima'],
+	    [5, 'Ely Ramos Yañe', '<eramos@tam-c.com>', 'Lima'],
+	]
 }
 
 // creamos documento
@@ -17,15 +28,13 @@ var workbook = new Excel.Workbook();
 var worksheet = workbook.addWorksheet('My Sheet', 'FFC0000');
 
 // definimos las columnas
-worksheet.columns = [
-    { header: 'Id', key: 'id', width: 10 },
-    { header: 'Name', key: 'name', width: 35 },
-    { header: 'Email', key: 'email', width: 20 },
-    { header: 'Ubigeo', key: 'ubigeo', width: 20 }
-];
+worksheet.columns = conf.COLS;
+
+// definimos las filas
+worksheet.addRows(conf.ROWS);
 
 // escribimos el documento, en un stream
-workbook.xlsx.write(fs.createWriteStream(conf.TO))
+workbook.xlsx.write(fs.createWriteStream(conf.PATH))
 	.then(function(){
 		console.log('creado');
 	})
